3110d96c44adffe42b56d1a9d1ce09d70b700445
Dinarte Jesus <jdinartejesus@gmail.com>
2015-03-14 16:19:28 +0000

    create file app.js and main.js and fix package.json

---
+++ src/js/components/app.jsx
@@ -0,0 +1,9 @@
+var react = require('react');
+
+var App = React.createClass({
+    render: function(){
+      return: <h1>MY APP</h1>
+    }
+});
+
+module.exports = App;
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+var App = require('./components/app');
+    React =require('react');
+
+React.render(<App /> document.getElementById('main'));
